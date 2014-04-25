87e6ce75b861b920ed1b97670617b930bb6e9298
Jan Erik Breimo <jebreimo@gmail.com>
2014-04-25 08:58:58 +0200

    Added test file.

---
+++ TestClass.cpp
@@ -0,0 +1,16 @@
+    size_t m_Assertions;
+    std::vector<Error> m_Errors;
+    std::string m_Name;
+    std::unique_ptr<Time> m_StartTime;
+    std::vector<std::pair<std::string, std::string>> m_Filter;
+
+@<T, int N>@@
+    std::vector<Error> m_Errors;
+    std::string m_Name;
+@Fjon
+    clock_t m_StartTime;
+@<T, int K>@@
+    clock_t m_EndTime;
+@<T>@@<T, Foo::UserId>
+    std::vector<Error> m_Errors;
+    std::string m_Name;
